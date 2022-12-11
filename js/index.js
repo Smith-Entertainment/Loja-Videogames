07c0cfa6ccda644549d3f7bbe5b7d140cb620ce2
Lerr1394 <87046180+Lerr1394@users.noreply.github.com>
2022-12-11 02:08:57 -0300

    Filtros

--- js/index.js
+++ js/index.js
@@ -1,14 +1,37 @@
+var quantJogos = 10;
+
+function somarJogos() {
+	quantJogos += 10;
+}
 
 function Mostjogos(jogos) {
+	document.getElementById("containerBanner").innerHTML = `<a href="${jogos[0].freetogame_profile_url}" id="freetogame_profile_url">
+	<div id="img_banner"><img src="${jogos[0].thumbnail}" id="thumbnail" alt=""></div>
+	<h4 id="title">${jogos[0].title}</h4>
+</a>`
 
-	for (i = 0; i < quantJogos; i++) {
-		document.getElementById("containerJogo").innerHTML = `<div id="game"> 
-		<h2> Titulo: ${jogos[i].title} </h2> <p> Genre: ${jogos[i].genre} </p>
-		 <img src="${jogos[i].thumbnail}"> </div>`;;
+	document.getElementById("containerJogo").innerHTML = '';
+	let buttonMostrarMais = document.getElementById("div_btn");
+	buttonMostrarMais.addEventListener('click', somarJogos);
+	for (i = 1; i < quantJogos; i++) {
+		console.log(quantJogos);
+		document.getElementById("containerJogo").innerHTML += `
+		<a href="${jogos[i].freetogame_profile_url}" id="freetogame_profile_url"> <div class="game" > <div><img src="${jogos[i].thumbnail}" id="thumbnail" alt=""></div>
+		<div id="alinhar_text_botao">
+			<h4 id="title">${jogos[i].title}</h4>
+			<button id="btn_favoritar"><span class="material-symbols-outlined">star</span></button>
+			<h4 id="genre"></h4>
+		<h4 id="platform"></h4>
+		</div>
+		
+		</div>
+		
+		</a>`;
 
 		//console.log(jogos[i]);
 	}
 }
+
 
 const verJogos = (category) => {
 	const options = {
@@ -23,7 +46,7 @@ const verJogos = (category) => {
 	fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?category=${category}`, options)
 		.then(response => response.json())
 		.then(response => {
-			
+
 			Mostjogos(response);
 		})
 		.catch(err => console.error(err));
@@ -32,4 +55,32 @@ const verJogos = (category) => {
 
 
 
+let buttonFantasy = document.getElementById("fantasy");
+buttonFantasy.addEventListener('click', () => verJogos("fantasy"));
 
+let buttonShooter = document.getElementById("shooter");
+buttonShooter.addEventListener('click', () => verJogos("Shooter"));
+
+let buttonSocial = document.getElementById("social");
+buttonSocial.addEventListener('click', () => verJogos("Social"));
+
+let buttonMMORPG = document.getElementById("MMORPG");
+buttonMMORPG.addEventListener('click', () => verJogos("MMORPG"));
+
+let buttonStrategy = document.getElementById("Strategy");
+buttonStrategy.addEventListener('click', () => verJogos("Strategy"));
+
+let buttonFighting = document.getElementById("Fighting");
+buttonFighting.addEventListener('click', () => verJogos("Fighting"));
+
+let buttonSports = document.getElementById("Sports");
+buttonSports.addEventListener('click', () => verJogos("Sports"));
+
+//let buttonPc = document.getElementById("pc");
+//buttonPc.addEventListener('click', () => verJogos("sports", "pc"));
+
+//let buttonBrowser = document.getElementById("browser");
+//buttonBrowser.addEventListener('click', () => verJogos("shooter", "browser"));
+
+//let buttonAll = document.getElementById("all");
+//buttonAll.addEventListener('click', () => verJogos("shooter", "all"));
